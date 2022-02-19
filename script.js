5d0b9ad30a07f2dead2f3b245bfce30c79b61a00
hasibulislam999 <hasib143sl@gmail.com>
2022-02-19 13:11:25 +0600

    calculator done

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
-const calcInput = document.getElementById('calc__input');
-const numericBtn = document.getElementsByClassName('numeric__btn');
-const opBtn = document.querySelectorAll('.op__btns button');
-let value1, value2;
-
-// one.addEventListener('click', function(){
-//     calcInput.value = 1;
-//     console.log(calcInput.value);
-// });
-
-for (const btn of numericBtn) {
-    btn.addEventListener('click', function () {
-        // calcInput.value = parseInt(btn.innerText);
-        // console.log(parseInt(calcInput.value));
-        // if(btn.innerText == '.')
-        //     console.log('.');
-        // value1 = btn.innerText;
-
-        calcInput.value += btn.innerText;
-    });
+let number1, number2, tempNum, result, operator, flag = false;
+function myCalculator(number1, number2, operator) {
+    if(operator == '+'){
+        result = number1 + number2;
+    }
+    else if(operator == '-'){
+        result = number1 - number2;
+    }
+    else if(operator == '*'){
+        result = number1 * number2;
+    }
+    else if(operator == '/'){
+        result = number1 / number2;
+    }
+    else{
+        window.alert('Wrong Input');
+    }
+    document.querySelector('.calculator-scrn').innerText = result;
 }
+document.querySelector('.calculator-btn').addEventListener('click', (event) => {
+    if(flag == true){
+        document.querySelector('.calculator-scrn').innerText = '';
+        flag = false;
+    }
+    if ((isNaN(event.target.innerText) == false && flag == false) || event.target.innerText == '.') {
+        document.querySelector('.calculator-scrn').innerText += event.target.innerText;
+        tempNum = (parseFloat(document.querySelector('.calculator-scrn').innerText));
+    }
+    else if(event.target.innerText == '='){
+        number2 = tempNum;
+        myCalculator(number1, number2, operator);
+        result = tempNum;
+    }
+    else{
+        operator = event.target.innerText;
+        number1 = tempNum;
+        document.querySelector('.calculator-scrn').innerText = '';
+        document.querySelector('.calculator-scrn').innerText = event.target.innerText;
+        flag = true;
+    }
+});
 
-for (const btn of opBtn) {
-    btn.addEventListener('click', function () {
-        // calcInput.value = parseInt(btn.innerText);
-        // console.log(parseInt(calcInput.value));
-        // if(btn.innerText == '.')
-        //     console.log('.');
-
-        calcInput.value = btn.innerText;
-    });
+document.querySelector('.calculator-scrn').onclick = function(){
+    document.querySelector('.calculator-scrn').innerText = '';
 }
-
-// for (const btn of numericBtn) {
-//     btn.addEventListener('click', function () {
-//         // calcInput.value = parseInt(btn.innerText);
-//         // console.log(parseInt(calcInput.value));
-//         // if(btn.innerText == '.')
-//         //     console.log('.');
-
-//         value2 = btn.innerText;
-//         calcInput.value += value2;
-//     });
-// }
